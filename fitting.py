f92878a56bbc8f1dab4e714c763429cdb8dd441d
Lisa McBride <lisaleemcb@gmail.com>
2022-08-17 15:03:19 -0400

    increased nsteps parameter for all MCMCs

--- fitting.py
+++ fitting.py
@@ -358,7 +358,7 @@ def Beane_et_al(spectra, P_N_i, P_N_j, P_N_k, N_modes, k_indices):
 
 def MCMC_results(params, k_indices, data, model, N, p0_in=None,
                 priors='gaussian', priors_width=.25, positivity=False,
-                pdf='gaussian', backend_filename=None, nsteps=1e6, nwalkers=72,
+                pdf='gaussian', backend_filename=None, nsteps=1e8, nwalkers=72,
                 burn_in=1e3, parallel=False):
     # lopping off the bias
     data_size = model.pspec(k_indices).size
