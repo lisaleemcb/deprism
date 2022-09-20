05656a2e8a09a4c3c83917dd4296a95db874e33b
Lisa McBride <lisaleemcb@gmail.com>
2022-09-20 12:45:10 -0700

    changed params to params_dict

--- analysis.py
+++ analysis.py
@@ -399,13 +399,13 @@ def inject_noise(data, N):
     return noisey_data
 
 def run_analysis(k_indices, spectra, params_dict, N_modes, frac_error, model):
-    N, n = get_noise(k_indices, spectra, params['b_i'], N_modes, frac_error=frac_error)
-    data = utils.fetch_data(k_indices, spectra, b_0=params['b_i'])
+    N, n = get_noise(k_indices, spectra, params_dict['b_i'], N_modes, frac_error=frac_error)
+    data = utils.fetch_data(k_indices, spectra, b_0=params_dict['b_i'])
     data_noise = inject_noise(data, N)
 
     Beane = fitting.Beane_et_al(data_noise, spectra, n[0], n[1], n[2], N_modes, k_indices)
     LSE = fitting.LSE_results(k_indices, data_noise, N)
-    MCMC = fitting.MCMC_results(params, k_indices, data_noise, model, N)
+    MCMC = fitting.MCMC_results(params_dict, k_indices, data_noise, model, N)
 
     return Beane, LSE, MCMC
 
