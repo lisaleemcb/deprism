5f6cb397cee344724bf24003a1c042c00ecd081d
Lisa McBride <lisaleemcb@gmail.com>
2022-09-23 17:36:21 -0700

    fixed bug where the wrong noise was added if the error_x keyword was True

--- analysis.py
+++ analysis.py
@@ -402,16 +402,16 @@ def inject_noise(data, N):
 
 def run_analysis(k_indices, spectra, params_dict, N_modes, frac_error, model,
                     error_x=True, priors_width=.10):
+
+    data = utils.fetch_data(k_indices, spectra, b_0=params_dict['b_i'])
     N, n = get_noise(k_indices, spectra, params_dict['b_i'], N_modes,
                                             frac_error=frac_error, priors_width=priors_width)
-    data = utils.fetch_data(k_indices, spectra, b_0=params_dict['b_i'])
-    data_noise = inject_noise(data, N)
 
-    print(np.diag(N))
+    data_noise = inject_noise(data, N)
 
     if error_x is True:
         N = estimate_errors(data, frac_error=frac_error, priors_width=priors_width)
-        print(np.diag(N))
+        data_noise = inject_noise(data, N)
 
     Beane = fitting.Beane_et_al(data_noise, spectra, n[0], n[1], n[2], N_modes, k_indices)
     LSE = fitting.LSE_results(k_indices, data_noise, N)
